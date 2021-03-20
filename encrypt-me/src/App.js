73ae21362cfe94f39f3fc2e5400e694a6d069a40
A N <foto.arn@gmail.com>
2021-03-19 21:20:37 -0700

    + Install reactRouter & styledComponents

--- encrypt-me/src/App.js
+++ encrypt-me/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Counter } from "./features/counter/Counter";
 import "./Css/App.css";
+import styled from "styled-components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
