454ee2fefc298a22ed4a4dcea51c226b64edc19a
A N <foto.arn@gmail.com>
2021-03-20 22:54:30 -0700

    + Add component folders

--- encrypt-me/src/Body/index.js
+++ encrypt-me/src/Body/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import FinanceWidget from "../Components/Finance";
+import FinanceWidget from "../Components/FinanceWidget";
+import JournalWidget from "../Components/JournalWidget";
 
 function Body() {
 	return (
 		<div>
 			<h1>App Body</h1>
 			<FinanceWidget />
+			<JournalWidget />
 		</div>
 	);
 }
---
+++ encrypt-me/src/Components/JournalWidget/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JournalWidget() {
+	return (
+		<div>
+			<h1>Journal Widget.</h1>
+		</div>
+	);
+}
+
+export default JournalWidget;
