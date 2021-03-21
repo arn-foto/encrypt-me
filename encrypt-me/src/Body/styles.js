029420a64ce16fe173514eaa661ca04495890aa8
A N <foto.arn@gmail.com>
2021-03-21 00:24:38 -0700

    + Setting up react-router

--- encrypt-me/src/Body/styles.js
+++ encrypt-me/src/Body/styles.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
 const AppBody = styled.div`
+	display: flex;
+	justify-content: center;
 	background-color: lightgray;
 `;
 
 const LinkBody = styled.div`
 	display: flex;
 	justify-content: space-evenly;
+
+	> a {
+		font-size: 2em;
+		color: red;
+		text-decoration: none;
+	}
 `;
 
 export { AppBody, LinkBody };
