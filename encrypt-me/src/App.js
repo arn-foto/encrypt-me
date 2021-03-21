4d947d1b1b399e58bd600306cb9990a940bea202
A N <foto.arn@gmail.com>
2021-03-20 23:04:09 -0700

    + Adjusting folder structure

--- encrypt-me/src/App.js
+++ encrypt-me/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Header from "./Header";
 import Body from "./Body";
-import "./Css/App.css";
-import { AppBody } from "../src/Body/styles";
 
 function App() {
 	return (
