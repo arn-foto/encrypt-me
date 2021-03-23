317b6faec9ca4e641fea7e899d59e5db9c46786a
A N <foto.arn@gmail.com>
2021-03-23 00:33:46 -0700

    + Configuring db

--- encrypt-me/src/firebase.js
+++ encrypt-me/src/firebase.js
@@ -1,18 +1,19 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+
+import "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCLgDuLYgbp_JGzXA4O7YfrR8qXpbECDtc",
-	authDomain: "encrypt-me-643c6.firebaseapp.com",
-	projectId: "encrypt-me-643c6",
-	storageBucket: "encrypt-me-643c6.appspot.com",
-	messagingSenderId: "942353904257",
-	appId: "1:942353904257:web:1ba53b204851c0cd469380",
-	measurementId: "G-FJCREP5LC5",
+	apiKey: `${process.env.APIKEY}`,
+	authDomain: `${process.env.AUTH_DOMAIN}`,
+	databaseURL: `${process.env.DATABASE_URL}`,
+	projectId: `${process.env.PROJECTID}`,
+	storageBucket: `${process.env.STORAGE_BUCKET}`,
+	messagingSenderId: `${process.env.MESSAGE_SENDER_ID}`,
+	appId: `${process.env.APP_ID}`,
+	measurementId: `${process.env.MEASUREMENT_ID}`,
 };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
-
-export { auth, provider, db };
+export default firebase;
