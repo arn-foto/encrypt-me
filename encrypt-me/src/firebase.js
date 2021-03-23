936b0be0419d4b374c4cb9a925f4c1b1b25b9c88
A N <foto.arn@gmail.com>
2021-03-23 00:41:39 -0700

    + Configuring db

--- encrypt-me/src/firebase.js
+++ encrypt-me/src/firebase.js
@@ -5,7 +5,6 @@ import "firebase/firestore";
 const firebaseConfig = {
 	apiKey: `${process.env.APIKEY}`,
 	authDomain: `${process.env.AUTH_DOMAIN}`,
-	databaseURL: `${process.env.DATABASE_URL}`,
 	projectId: `${process.env.PROJECTID}`,
 	storageBucket: `${process.env.STORAGE_BUCKET}`,
 	messagingSenderId: `${process.env.MESSAGE_SENDER_ID}`,
