9bb21e4f5659a2a5e969a1c848f8f575d1b0af46
A N <foto.arn@gmail.com>
2021-03-23 01:25:45 -0700

    + Create AddNote function

--- encrypt-me/src/Components/JournalWidget/AddNote.js
+++ encrypt-me/src/Components/JournalWidget/AddNote.js
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
+import firebase from "./firebase";
 
 const AddNote = () => {
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
 
-	return <div></div>;
+	const addNote = () => {
+		firebase.firestore().collection("notes").add({
+			title,
+			body,
+		});
+
+		setTitle("");
+		setBody("");
+	};
+
+	return (
+		<AddNoteDiv>
+			<InputTitle value={title} onChange={(e) => setTitle(e.target.value)} />
+			<BodyTextArea value={body} onChange={(e) => setBody(e.target.value)} />
+			<Button onClick={addNote}>Add Note</Button>
+		</AddNoteDiv>
+	);
 };
 
 export default AddNote;
