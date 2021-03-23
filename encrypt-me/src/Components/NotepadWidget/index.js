3122e4f940be94c9df2494d71ba917e9d991cfc4
A N <foto.arn@gmail.com>
2021-03-22 23:27:56 -0700

    + Add input

--- encrypt-me/src/Components/NotepadWidget/index.js
+++ encrypt-me/src/Components/NotepadWidget/index.js
@@ -5,8 +5,9 @@ function NotepadWidget() {
 	return (
 		<div>
 			<NotepadContainer>
-				<h1>Notepad Widget.</h1>
+				<h1>Notepad.</h1>
 				<input placeholder="New note" />
+				<textarea />
 			</NotepadContainer>
 		</div>
 	);
