0db8d06a79b9931a7c1321f46b02ffcfaf1bc603
A N <foto.arn@gmail.com>
2021-03-24 02:49:29 -0700

    + Add calendar to notes

--- encrypt-me/src/Body/styles.js
+++ encrypt-me/src/Body/styles.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const AppBody = styled.div`
 	display: flex;
 	justify-content: center;
-	background-color: whitesmoke;
 `;
 
 const LinkBody = styled.div`
--- encrypt-me/src/Components/NotepadWidget/styles.js
+++ encrypt-me/src/Components/NotepadWidget/styles.js
@@ -117,8 +117,13 @@ const ListItemDeleteButton = styled.button`
 
 const CalendarStyle = styled.div`
 	width: 300px;
-
+	background-color: #afe0cc;
 	border-radius: 2px;
+
+	:hover {
+		box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.24);
+		cursor: pointer;
+	}
 `;
 
 export {
