a636298feefc7e374bda917cd5e434aedf3ac5a1
A N <foto.arn@gmail.com>
2021-04-13 11:13:33 -0700

    + Fix journal flexbox

--- encrypt-me/src/Components/JournalWidget/styles.js
+++ encrypt-me/src/Components/JournalWidget/styles.js
@@ -3,7 +3,13 @@ import styled from "styled-components";
 const JournalContainer = styled.div`
 	display: flex;
 	flex-direction: row;
-	background-color: lightseagreen;
+	background-color: whitesmoke;
+	background: #afe0cc;
+	box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.24);
+	padding: 20px;
+	margin: 21px;
+	width: 150vh;
+	height: 100vh;
 `;
 
 export { JournalContainer };
