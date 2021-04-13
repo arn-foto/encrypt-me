9d1be3272238cac3379c652511819d6e25d18db1
A N <foto.arn@gmail.com>
2021-04-13 11:42:45 -0700

    + Clean up App.js

--- encrypt-me/src/App.js
+++ encrypt-me/src/App.js
@@ -2,20 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
-import styled from "styled-components";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<BodyContainer>
-				<Body />
-			</BodyContainer>
+			<Body />
 			<Footer />
 		</div>
 	);
 }
 
 export default App;
-
-const BodyContainer = styled.div``;
