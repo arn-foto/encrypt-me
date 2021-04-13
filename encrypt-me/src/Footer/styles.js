d7493713b368d9efae26f3074b8079b21f99e2b2
A N <foto.arn@gmail.com>
2021-04-12 20:16:39 -0700

    + Add footer

---
+++ encrypt-me/src/Footer/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+	display: flex;
+	background-color: whitesmoke;
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+`;
+
+export { FooterContainer };
