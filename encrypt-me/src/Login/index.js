bc06451f5319046c23ce73e6441b85735c753632
A N <foto.arn@gmail.com>
2021-04-13 20:32:51 -0700

    + Add user config in Redux store

---
+++ encrypt-me/src/Login/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginContainer from "./styles";
+
+const Login = () => {
+	return (
+		<div>
+			<LoginContainer>
+				<h1>Login</h1>
+			</LoginContainer>
+		</div>
+	);
+};
+
+export default Login;
---
+++ encrypt-me/src/Login/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const LoginContainer = styled.div`
+	background-color: whitesmoke;
+	padding: 50px;
+	margin: 50px;
+`;
+export default { LoginContainer };
