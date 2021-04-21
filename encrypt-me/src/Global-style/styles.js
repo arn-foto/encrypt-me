9c56d8309a7a8d2f18a870bff43d1d2635ed755f
A N <foto.arn@gmail.com>
2021-04-21 12:02:06 -0700

    + Add global container

---
+++ encrypt-me/src/Global-style/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const GlobalContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	background-color: whitesmoke;
+	background: #afe0cc;
+	box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.24);
+	padding: 20px;
+	margin: 21px;
+	width: 150vh;
+	height: 100vh;
+`;
+
+export { GlobalContainer };
