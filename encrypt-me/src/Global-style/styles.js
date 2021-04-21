80ba6b933ee3968fcc65f851086df658cb2a2e2d
A N <foto.arn@gmail.com>
2021-04-21 12:17:55 -0700

    + Add global container

--- encrypt-me/src/Global-style/styles.js
+++ encrypt-me/src/Global-style/styles.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
-// this is the main container that holds all of the components. This is so I don't have to repeat the same code in other folders. One stop shop!
+// this is the main container that holds all of the components.
+// This is so I don't have to repeat the same code in other folders.
+// One stop shop!
+
 const GlobalContainer = styled.div`
 	background-color: whitesmoke;
 	background: lightblue;
-
 	box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.24);
 	padding: 20px;
 	margin: 21px;
