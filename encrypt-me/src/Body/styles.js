c4f1fce8b305985ba1d88d2798b42d7dd45b6e44
A N <foto.arn@gmail.com>
2021-04-21 13:02:21 -0700

    + Add glass effect

--- encrypt-me/src/Body/styles.js
+++ encrypt-me/src/Body/styles.js
@@ -4,8 +4,10 @@ const AppBody = styled.div`
 	display: flex;
 	flex-direction: row;
 	background-color: lightcyan;
-	background: url(https:images.unsplash.com/photo-1544306094-e2dcf9479da3)
+	background: url(https:images.unsplash.com/photo-1470217407524-b1e77afc6ec5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1046&q=80)
 		no-repeat;
+	background-attachment: fixed;
+	background-size: cover;
 `;
 
 const LinkBody = styled.div`
