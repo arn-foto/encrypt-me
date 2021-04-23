77a8a3e3883f69c8ab66da4ab8a57fea5b7f7ca2
A N <foto.arn@gmail.com>
2021-04-23 00:12:46 -0700

    + Notepad style

--- encrypt-me/src/Global-style/styles.js
+++ encrypt-me/src/Global-style/styles.js
@@ -9,8 +9,8 @@ const GlobalContainer = styled.div`
 	backdrop-filter: blur(5px);
 	box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.24);
 	padding: 20px;
-	margin: 21px;
-	width: 150vh;
+	margin: 20px;
+
 	height: 100vh;
 `;
 
